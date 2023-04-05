af7e784327ebfec7348224f84c652287b3c5721c
Vladimir Zhelvis <v.zhelvis@adguard.com>
2023-04-05 23:43:51 +0300

    update deps, speed up test runner with swc

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,14 @@
-module.exports = {
+import { Config } from "jest";
+
+const config: Config = {
   roots: ["<rootDir>/src"],
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
   ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": "@swc/jest",
   },
 };
+
+export default config;
